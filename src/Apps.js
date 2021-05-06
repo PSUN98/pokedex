b0f90b054fc549907a5b26b22e6612148d125a36
PSUN98 <psyq98@gmail.com>
2021-05-06 10:34:42 +0800

    change homepage

---
+++ src/Apps.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import App from './App';
+
+class Apps extends Component {
+
+    render() {
+        return (
+            <div>
+                <App>
+
+                </App>
+
+            </div>
+        );
+    }
+}
+
+export default Apps;
