8a6c869abc07e3e210ea208896f7bd0a691f6ac7
PSUN98 <psyq98@gmail.com>
2021-05-06 15:45:06 +0800

    Added restart button

--- src/Apps.js
+++ src/Apps.js
@@ -30,9 +30,23 @@ class Apps extends Component {
 
         if (battle[0] > battle[1]) {
             this.setState({ player1win: true })
-        } else {
+        } else if (battle[0] < battle[1]) {
             this.setState({ player2win: true })
+        } else if (battle[0] === battle[1]) {
+            this.setState({
+                player1win: true,
+                player2win: true
+            })
         }
+    }
+
+    handleRestart = () => {
+        battle = [];
+        this.setState({
+            player1win: false,
+            player2win: false
+        })
+
     }
 
 
@@ -42,6 +56,12 @@ class Apps extends Component {
     render() {
         return (
             <div>
+                <span style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    // backgroundColor: 'lightGrey'
+                }}>Player 1 enter battle first. Player 2 enter battle second.</span>
 
 
 
@@ -49,7 +69,7 @@ class Apps extends Component {
                     display: 'flex',
                     flexDirection: 'row',
                     justifyContent: 'space-evenly',
-                    //backgroundColor: 'lightGrey'
+                    backgroundColor: 'lightGrey'
                 }}>
 
                     {this.state.apps.map(app =>
@@ -59,17 +79,25 @@ class Apps extends Component {
 
 
                         >
-                            <h4>Player #{app.id}</h4>
+                            <h4>Player {app.id}</h4>
 
                         </App>
                         //attribute: onStart,app that is set here is passed to component <App/> using a single js object called props
                         //special prop: children is used when we want to pass content in between opening and closing tag of an element 
                     )}
                 </div>
-                <button className="btn btn-primary m-2" onClick={this.handleGame} >Start Game</button>
-                {this.state.player1win && this.state.player2win ? <h1>draw</h1> :
-                    this.state.player1win && !this.state.player2win ? <h1>player 1 win</h1> : !this.state.player1win && this.state.player2win ?
-                        <h1>player 2 win</h1> : <h1>start game</h1>}
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    // backgroundColor: 'lightGrey'
+                }}>
+                    <button className="btn btn-primary m-2" onClick={this.handleRestart} >Restart Game</button>
+                    <button className="btn btn-primary m-2" onClick={this.handleGame} >Start Game</button>
+                    {this.state.player1win && this.state.player2win ? <h1>Draw</h1> :
+                        this.state.player1win && !this.state.player2win ? <h1>Player 1 win</h1> : !this.state.player1win && this.state.player2win ?
+                            <h1>Player 2 win</h1> : <h1>Start Game</h1>}
+                </div>
 
 
 
